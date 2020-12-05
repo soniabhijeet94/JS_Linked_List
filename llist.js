56e50f5dfad408cf12728414cb6d66db6cfd68d4
asoni94 <soniabhijeet94@yahoo.com>
2020-12-06 03:17:05 +0530

    Small logging tweaks.

--- llist.js
+++ llist.js
@@ -51,13 +51,17 @@ class LL {
 
 
 var obj = new LL();
+
+console.log("Print initial list")
 obj.print_list();
 console.log();
 
+console.log("Add 5 to start")
 obj.add_to_start(5);
 obj.print_list();
 console.log();
 
+console.log("Add 23 to start")
 obj.add_to_start(23);
 obj.print_list();
 
